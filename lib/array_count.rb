7e46f393c7891e22ffd271cdcfe99e6c1304fde8
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-30 00:45:09 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,7 @@
 def count_strings(array)
+  disney_movies = ["Aladdin", "Beauty and the Beast", "Cinderella", "Dumbo". "Enchanted", "Frozen"]
   # Return the total number of strings in the provided array using the count enumerable
+ p disney_movies.count
 end
 
 def count_empty_strings(array)
